d7564433d1fa9e441679f0de2accd7dc8eaab5e6
Kamille Longalong <k.pagibigan@gmail.com>
2020-05-14 02:34:07 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,7 +10,7 @@ def assign_rooms(attendees)
   attendees.map.with_index { |name, number| "Hello, #{name}! You'll be assigned to room #{number + 1}!"}
 end
 
-def printer(attendees)
-  puts batch_badge_creator
-  puts assign_rooms
+def printer
+  puts batch_badge_creator(attendees)
+  puts assign_rooms(attendees)
 end
